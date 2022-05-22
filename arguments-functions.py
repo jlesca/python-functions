234dcbcfb2595938f8a25873c4e75b5f40414e99
José Lescano <92216756+jlesca@users.noreply.github.com>
2022-05-22 11:10:07 -0300

    Create arguments-functions.py

---
+++ arguments-functions.py
@@ -0,0 +1,19 @@
+# You can pass argument(s) into the function by putting them inside the parenthesis.
+
+def myFunction(fruit):
+    print("I like " + fruit)
+    
+# When the function is called, then you need to specify a value for the arguments:
+
+myFunction("banana")
+myFunction("apple")
+myFunction("Orange")
+
+# We can set if your function 2 or more arguments, but you have to call the function with the correct number of arguments, not more, and not less.
+
+def addNum(num1, num2):
+    print(num1 + num2)
+    
+addNum(2, 4)
+
+# Output: 6
