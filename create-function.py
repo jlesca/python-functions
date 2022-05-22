074f5708720d798819ad02e00fa9fd96f386d8d3
José Lescano <92216756+jlesca@users.noreply.github.com>
2022-05-22 11:01:12 -0300

    Update create-function.py

--- create-function.py
+++ create-function.py
@@ -1,1 +1,8 @@
+# We've to use the DEF keyword to set a function.
 
+# Then set a name to our function (myFunction):
+
+def myFunction():
+    print("Hello World from a function") # Statement to myFunction
+
+myFunction() # Call the function and it'll display the statement defined 
