3db03d7368d9ef37c914ff25f8bbe9a086c0cb65
José Lescano <92216756+jlesca@users.noreply.github.com>
2022-05-22 11:32:31 -0300

    Create return-values.py

---
+++ return-values.py
@@ -0,0 +1,11 @@
+# The return statement can contain an expression to execute once the function is called.
+
+# I defined a function named multiplyFunction and passed it "num1" as an argument.
+def multiplyFuntion(num1):
+    return num1 * 5 # I used the return keyword to specify that I want num1 to be multiplied by 5
+
+# I called the function, passed different numbers into it as the value for the num1 argument
+
+print(multiplyFuntion(5))
+print(multiplyFuntion(7))
+print(multiplyFuntion(9))
